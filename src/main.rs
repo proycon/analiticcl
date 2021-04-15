10415b27b0777e1191a484477965cadc5e23f936
Maarten van Gompel <proycon@anaproy.nl>
2021-04-15 21:46:21 +0200

    renamed tree to index

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 extern crate num_bigint;
 
-use std::collections::{HashMap,HashSet,BTreeSet,VecDeque,BinaryHeap};
+use std::collections::{HashMap,HashSet,VecDeque,BinaryHeap};
 use std::fs::File;
 use std::io::{Write,Read,BufReader,BufRead,Error};
 use std::ops::Deref;
@@ -16,7 +16,7 @@ type VocabId = u64;
 ///A normalized string encoded via the alphabet
 type NormString = Vec<u8>;
 
-const PRIMES: &[usize] = &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
+const PRIMES: &[u32] = &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
 
 #[derive(Clone)]
 struct VocabValue {
@@ -44,7 +44,7 @@ type Alphabet = Vec<Vec<String>>;
 
 
 #[derive(Default)]
-struct AnaTreeNode {
+struct AnaIndexNode {
     ///Maps an anagram value to all existing instances that instantiate it
     instances: Vec<VocabId>,
 
@@ -56,7 +56,7 @@ struct AnaTreeNode {
 }
 
 
-type AnaTree = HashMap<AnaValue,AnaTreeNode>;
+type AnaIndex = HashMap<AnaValue,AnaIndexNode>;
 
 struct VariantModel {
     decoder: VocabDecoder,
@@ -64,7 +64,7 @@ struct VariantModel {
 
     alphabet: Alphabet,
 
-    tree: AnaTree,
+    index: AnaIndex,
 
     ///Does the model have frequency information?
     have_freq: bool,
@@ -187,7 +187,7 @@ impl Anahash for AnaValue {
     }
 
     /// The value of an empty anahash
-    /// Also corresponds to the root of the tree
+    /// Also corresponds to the root of the index
     fn empty() -> AnaValue {
         AnaValue::one()
     }
@@ -263,14 +263,14 @@ struct AncestorIterator<'a> {
     alphabet_size: usize,
     queue: VecDeque<(AnaValue,usize)>, //(child,parent,depth)
     visited: HashSet<AnaValue>,
-    tree: Option<&'a AnaTree>,
+    index: Option<&'a AnaIndex>,
 }
 
 impl<'a> AncestorIterator<'a> {
-    fn new(anavalue: AnaValue, tree: Option<&'a AnaTree>, alphabet_size: usize) -> Self {
+    fn new(anavalue: AnaValue, index: Option<&'a AnaIndex>, alphabet_size: usize) -> Self {
         Self {
             alphabet_size: alphabet_size,
-            tree: tree,
+            index: index,
             queue: VecDeque::from(vec!((anavalue, 0))),
             visited: HashSet::new(),
         }
@@ -295,9 +295,9 @@ impl<'a> Iterator for AncestorIterator<'a>
     fn next(&mut self) -> Option<Self::Item> {
         //Pop the next time to consider from the queue
         if let Some((anahash, depth)) = self.queue.pop_front() {
-            //Do not expand items that already have parents in the tree
-            let expand = if let Some(tree) = self.tree {
-                if let Some(node) = tree.get(&anahash) {
+            //Do not expand items that already have parents in the index
+            let expand = if let Some(index) = self.index {
+                if let Some(node) = index.get(&anahash) {
                     node.parents.is_empty()
                 } else {
                     true
@@ -443,7 +443,7 @@ impl VariantModel {
             alphabet: Vec::new(),
             encoder: HashMap::new(),
             decoder: Vec::new(),
-            tree: HashMap::new(),
+            index: HashMap::new(),
             have_freq: false,
             debug: debug,
         };
@@ -452,12 +452,12 @@ impl VariantModel {
         model
     }
 
-    fn get_or_create_node<'a,'b>(&'a mut self, anahash: &'b AnaValue) -> &'a mut AnaTreeNode {
+    fn get_or_create_node<'a,'b>(&'a mut self, anahash: &'b AnaValue) -> &'a mut AnaIndexNode {
             if self.contains_key(anahash) {
-                self.tree.get_mut(anahash).expect("get_mut on node after check")
+                self.index.get_mut(anahash).expect("get_mut on node after check")
             } else {
-                self.tree.insert(anahash.clone(), AnaTreeNode::default());
-                self.tree.get_mut(&anahash).expect("get_mut on node after insert")
+                self.index.insert(anahash.clone(), AnaIndexNode::default());
+                self.index.get_mut(&anahash).expect("get_mut on node after insert")
             }
     }
 
@@ -467,7 +467,7 @@ impl VariantModel {
         let alphabet_size = self.alphabet.len();
 
         // Hash all strings in the lexicon
-        // and add them to the tree
+        // and add them to the index
         let mut tmp_hashes: Vec<(AnaValue,VocabId)> = Vec::with_capacity(self.decoder.len());
         for (id, value)  in self.decoder.iter().enumerate() {
             //get the anahash
@@ -479,13 +479,13 @@ impl VariantModel {
         }
         eprintln!(" - Found {} instances",tmp_hashes.len());
 
-        eprintln!("Adding all instances to the tree");
+        eprintln!("Adding all instances to the index");
         for (anahash, id) in tmp_hashes {
-            //add it to the tree
+            //add it to the index
             let node = self.get_or_create_node(&anahash);
             node.instances.push(id);
         }
-        eprintln!(" - Found {} anagrams", self.tree.len() );
+        eprintln!(" - Found {} anagrams", self.index.len() );
 
         self.compute_deletions(2);
 
@@ -493,7 +493,7 @@ impl VariantModel {
 
         // Insertions are simply the reverse of deletions
         let mut insertions: HashMap<AnaValue,Vec<AnaValue>> = HashMap::new();
-        for (anahash, node) in self.tree.iter() {
+        for (anahash, node) in self.index.iter() {
             for parent in node.parents.iter() {
                 if let Some(insertions) = insertions.get_mut(&parent) {
                     insertions.push(anahash.clone());
@@ -511,7 +511,7 @@ impl VariantModel {
         eprintln!("Sorting node values...");
 
         // Sort the insertions in a separate step
-        for (_, node) in self.tree.iter_mut() {
+        for (_, node) in self.index.iter_mut() {
             node.parents.sort_unstable();
             node.children.sort_unstable();
         }
@@ -526,7 +526,7 @@ impl VariantModel {
 
         let alphabet_size = self.alphabet.len();
 
-        let mut queue: Vec<AnaValue> = Vec::from_iter(self.tree.keys().map(|x| x.clone()));
+        let mut queue: Vec<AnaValue> = Vec::from_iter(self.index.keys().map(|x| x.clone()));
         let mut parents: HashMap<AnaValue,Vec<AnaValue>> = HashMap::new();
 
         if self.debug {
@@ -574,7 +574,7 @@ impl VariantModel {
             node.parents = parents;
         }
 
-        eprintln!(" - Expanded to {} anagrams", self.tree.len() );
+        eprintln!(" - Expanded to {} anagrams", self.index.len() );
     }
 
 
@@ -598,11 +598,11 @@ impl VariantModel {
 
 
     fn contains_key(&self, key: &AnaValue) -> bool {
-        self.tree.contains_key(key)
+        self.index.contains_key(key)
     }
 
     fn has_instances(&self, key: &AnaValue) -> bool {
-        if let Some(node) = self.tree.get(key) {
+        if let Some(node) = self.index.get(key) {
             !node.instances.is_empty()
         } else {
             false
