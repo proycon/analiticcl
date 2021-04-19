1af7213fd2436170d96c281ca79a6f67ee8b9ddb
Maarten van Gompel <proycon@anaproy.nl>
2021-04-19 19:37:04 +0200

    reorganized benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -19,48 +19,122 @@ pub fn benchmarks(c: &mut Criterion) {
 
     group.finish();
 
-    let mut group = c.benchmark_group("anahash_insertion_benchmark");
+    let mut group = c.benchmark_group("anahash_edit");
 
     let input_avs: Vec<AnaValue> = inputs.iter().map(|input| input.anahash(&alphabet)).collect();
     let change: AnaValue = "change".anahash(&alphabet);
 
     for (input_av, input) in input_avs.iter().zip(inputs) {
         group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(BenchmarkId::new("anahash",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
-            input_av.insert(black_box(&change));
+        group.bench_with_input(BenchmarkId::new("insert",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.insert(black_box(&change))
+        }));
+
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("contains",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.contains(black_box(&change))
+        }));
+
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("delete",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.delete(black_box(&change))
+        }));
+
+    }
+
+    group.finish();
+
+    let searchparams = SearchParams {
+        ..Default::default()
+    };
+
+    let searchparams_bfs = SearchParams {
+        breadthfirst: true,
+        allow_duplicates: false,
+        allow_empty_leaves: false,
+        ..Default::default()
+    };
+
+    let inputs: &[&str] = &["rat","houses","benchmarking"];
+
+    let mut group = c.benchmark_group("anahash_iter");
+    for (input_av, input) in input_avs.iter().zip(inputs) {
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("parents",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_parents(alphabet.len() as u8).count()
+        }));
+
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("singlebeam",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter(alphabet.len() as u8).count()
+        }));
+
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("recursive_bfs_nodups",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_recursive(alphabet.len() as u8, &searchparams_bfs).count()
         }));
     }
 
     group.finish();
 
-    let mut group = c.benchmark_group("anahash_contains_benchmark");
 
-    let input_avs: Vec<AnaValue> = inputs.iter().map(|input| input.anahash(&alphabet)).collect();
-    let change: AnaValue = "change".anahash(&alphabet);
+    /*
+    let mut group = c.benchmark_group("anahash_iter_parents_deletions_recursive_dfs");
 
     for (input_av, input) in input_avs.iter().zip(inputs) {
         group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(BenchmarkId::new("anahash",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
-            input_av.contains(black_box(&change));
+        group.bench_with_input(BenchmarkId::new("anahash_iter_parent_deletions_recursive_dfs",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_recursive(alphabet.len() as u8, &searchparams).count()
         }));
     }
-
     group.finish();
 
-    let mut group = c.benchmark_group("anahash_deletion_benchmark");
-
-    let input_avs: Vec<AnaValue> = inputs.iter().map(|input| input.anahash(&alphabet)).collect();
-    let change: AnaValue = "change".anahash(&alphabet);
+    let mut group = c.benchmark_group("anahash_iter_parents_deletions_recursive_bfs");
 
     for (input_av, input) in input_avs.iter().zip(inputs) {
         group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(BenchmarkId::new("anahash",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
-            input_av.delete(black_box(&change));
+        group.bench_with_input(BenchmarkId::new("anahash_iter_parent_deletions_recursive_bfs",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_recursive(alphabet.len() as u8, &searchparams).count()
+        }));
+    }
+    group.finish();
+
+    let mut group = c.benchmark_group("anahash_iter_parents_deletions_recursive_dfs_nodups");
+
+    let searchparams = SearchParams {
+        allow_duplicates: false,
+        allow_empty_leaves: false,
+        ..Default::default()
+    };
+
+    for (input_av, input) in input_avs.iter().zip(inputs) {
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("anahash_iter_parent_deletions_recursive_dfs_nodups",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_recursive(alphabet.len() as u8, &searchparams).count()
         }));
     }
 
     group.finish();
 
+    let mut group = c.benchmark_group("anahash_iter_parents_deletions_recursive_bfs_nodups");
+
+    let searchparams = SearchParams {
+        breadthfirst: true,
+        allow_duplicates: false,
+        allow_empty_leaves: false,
+        ..Default::default()
+    };
+
+    for (input_av, input) in input_avs.iter().zip(inputs) {
+        group.throughput(Throughput::Bytes(input.len() as u64));
+        group.bench_with_input(BenchmarkId::new("anahash_iter_parent_deletions_recursive_bfs_nodups",format!("input {} chars",input.chars().count())), &input_av, |b, input_av| b.iter(||{
+            input_av.iter_recursive(alphabet.len() as u8, &searchparams).count()
+        }));
+    }
+
+    group.finish();
+
+    */
 
     let simple_lexicon: &[&str] = &["rites","tiers", "tires","tries","tyres","rides","brides","dire"];
 
